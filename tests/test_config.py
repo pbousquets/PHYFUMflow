9ce3b920539472009dd306bf9aba43997efbf670
Pablo Bousquets-Muñoz <bousquetspablo@uniovi.es>
2025-03-06 11:06:55 +0000

    Added tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,16 +3,12 @@ from pathlib import Path
 import snakemake
 import src.phyfum.workflow.aux.checkConfig as checkConfig
 
+# Define the base directory for your project
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-def test_defaults(defaults: Path):
+def test_defaults():
+    defaults = BASE_DIR / "src/phyfum/workflow/config.schema.yml"
     cleanConfig = checkConfig.readDefaults(defaults)
 
     assert isinstance(cleanConfig, dict)
     assert len(cleanConfig) > 0
-
-
-def test_config_cleanup(defaults: Path, example_config: Path):
-    DEFAULTS = checkConfig.readDefaults(defaults)
-    config = snakemake.common.configfile.load_configfile(example_config)
-    finalConfig = checkConfig.makeConfig(config, DEFAULTS)
-    assert len(finalConfig) > 3 and finalConfig.get("mle_iterations") == 7500
